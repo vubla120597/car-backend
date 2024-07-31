117f726886ae690806d058dd935bb7c6beac0c12
Bui Vu <buivu120597@gmail.com>
2024-07-31 10:51:10 +0700

    Init project

---
+++ src/test/java/carbackend/CarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package carbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
